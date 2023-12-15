49b53160141af9d785096f1eab346cdcaf259702
DVitushkin <d.vitushkin@mk.dunice.net>
2023-12-15 16:51:55 +0300

    Add Task entity model

---
+++ src/main/java/DVitushkin/Vitushkin_Todo_List/models/Task.java
@@ -0,0 +1,29 @@
+package DVitushkin.Vitushkin_Todo_List.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@Table(name = "tasks")
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private boolean status;
+    private String text;
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedBy
+    private LocalDateTime updatedAt;
+}
