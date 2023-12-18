06b918c262141266e3b14e7d789ecf2202145af3
DVitushkin <d.vitushkin@mk.dunice.net>
2023-12-18 17:50:50 +0300

    Add package to error handling in project

--- src/main/java/DVitushkin/Vitushkin_Todo_List/controller/TaskController.java
+++ src/main/java/DVitushkin/Vitushkin_Todo_List/controller/TaskController.java
@@ -10,13 +10,7 @@ import DVitushkin.Vitushkin_Todo_List.response.CustomSuccessResponse;
 import DVitushkin.Vitushkin_Todo_List.service.TaskService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class TaskController {
@@ -74,18 +68,5 @@ public class TaskController {
     public BaseSuccessResponse deleteTaskById(@PathVariable("id") int id) {
         service.deleteTaskById(id);
         return new BaseSuccessResponse(200, true);
-    }
-
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(
-            MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
     }
 }
